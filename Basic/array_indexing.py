2a1cde529fcaadbd2fdef310ba84794a5e3210f3
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-01 20:42:46 +0545

    Refactor array indexing examples and add new slicing examples for 1-D and 2-D NumPy arrays

--- Basic/array_indexing.py
+++ Basic/array_indexing.py
@@ -3,21 +3,21 @@ Access Array Elements
 Example: Get the first element from the following array:
 """
 
-# import numpy as np
-# arr = np.array([1, 2, 3, 4])
-# print(arr[0])
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[0])
 
 # Example: Get the second element from the following array.
 
-# import numpy as np
-# arr = np.array([1, 2, 3, 4])
-# print(arr[1])
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[1])
 
 # Example: Get third and fourth elements from the following array and add them.
 
-# import numpy as np
-# arr = np.array([1, 2, 3, 4])
-# print(arr[2] + arr[3])
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[2] + arr[3])
 
 
 """
@@ -25,15 +25,15 @@ Access 2-D Arrays
 Example: Access the 2nd element on 1st dim:
 """
 
-# import numpy as np
-# arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-# print('2nd element on 1st dim: ', arr[0, 1])
+import numpy as np
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('2nd element on 1st dim: ', arr[0, 1])
 
 # Example: Access the 5th element on 2nd dim:
 
-# import numpy as np
-# arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
-# print('5th element on 2nd dim: ', arr[1, 4])
+import numpy as np
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('5th element on 2nd dim: ', arr[1, 4])
 
 
 
@@ -42,9 +42,9 @@ Access 3-D Arrays
 Example: Access the third element of the second array of the first array:
 """
 
-# import numpy as np
-# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-# print(arr[0, 1, 2])
+import numpy as np
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(arr[0, 1, 2])
 
 
 """
---
+++ Basic/array_slicing.py
@@ -0,0 +1,69 @@
+"""
+Slicing arrays
+Example: Slice elements from index 1 to index 5 from the following array:
+"""
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
+
+# Example: Slice elements from index 4 to the end of the array:
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[4:])
+
+# Example: Slice elements from the beginning to index 4 (not included):
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[:4])
+
+
+"""
+Negative Slicing
+Example: Slice from the index 3 from the end to index 1 from the end:
+"""
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[-3:-1])
+
+
+"""
+STEP
+Example: Return every other element from index 1 to index 5:
+"""
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5:2])
+
+# Example: Return every other element from the entire array:
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[::2])
+
+
+
+"""
+Slicing 2-D Arrays
+Example: From the second element, slice elements from index 1 to index 4 (not included):
+"""
+
+import numpy as np
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[1, 1:4])
+
+# Example: From both elements, return index 2:
+
+import numpy as np
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[0:2, 2])
+
+# Example: From both elements, slice index 1 to index 4 (not included), this will return a 2-D array:
+
+import numpy as np
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[0:2, 1:4])
