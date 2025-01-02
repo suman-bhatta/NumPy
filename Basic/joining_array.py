8488eccd35b7697aa23a55640dc78053c9ea8073
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-02 15:35:40 +0545

    Add examples for joining and stacking NumPy arrays using concatenate, stack, hstack, vstack, and dstack

---
+++ Basic/joining_array.py
@@ -0,0 +1,81 @@
+""""
+Joining NumPy Arrays
+Exmple: Join two arrays:
+"""
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.concatenate((arr1, arr2))
+
+print(arr)
+
+
+
+"""
+Joining Arrays Using Stack Functions
+Example: Stack two arrays:
+"""
+
+import numpy as np 
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis=1)
+
+print(arr)
+
+
+"""
+Stacking Along Rows
+Exmple: Stack two arrays along rows:
+"""
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.hstack((arr1, arr2))
+
+print(arr)
+
+
+
+"""
+Stacking Along Columns
+Example: Stack two arrays along columns:
+"""
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.vstack((arr1, arr2))
+
+print(arr)
+
+
+
+"""
+Stacking Along Height (depth)
+Example: Stack two arrays along height (depth):
+"""
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.dstack((arr1, arr2))
+
+print(arr)
