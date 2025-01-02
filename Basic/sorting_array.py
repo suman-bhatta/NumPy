73bfcfc82677b181ef3ae89ea1b31f292a695e74
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-02 16:13:40 +0545

    Add examples for sorting 1-D and 2-D NumPy arrays

---
+++ Basic/sorting_array.py
@@ -0,0 +1,40 @@
+"""
+Sorting Arrays
+Example: Sort the array:
+"""
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))
+
+
+
+# Example: Sort the array alphabetically:
+
+import numpy as np
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+
+
+# Example: Sort a boolean array:
+
+import numpy as np
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
+
+
+"""
+Sorting a 2-D Array
+Example: Sort a 2-D array:
+"""
+
+import numpy as np 
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
