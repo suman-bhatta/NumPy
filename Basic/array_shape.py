a67f5b3f792a23727a9b89112155a801751f0da5
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-02 07:41:09 +0545

    Add examples for printing the shape of 2-D and multi-dimensional NumPy arrays

---
+++ Basic/array_shape.py
@@ -0,0 +1,20 @@
+"""
+Shape of an array
+Exmple: Print the shape of a 2-D array:
+"""
+
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape)
+
+
+# Example: Create an array with 5 dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('shape of array :', arr.shape)
