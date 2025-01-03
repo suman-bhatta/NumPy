4face28a46f1a19d7849634738f9aa2d0a011983
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-03 07:33:43 +0545

    Add examples for generating and visualizing binomial distributions using NumPy

---
+++ Random/binomial_distribution.py
@@ -0,0 +1,38 @@
+"""
+Binomial Distribution
+Example: Given 10 trials for coin toss generate 10 data points.
+"""
+
+from numpy import random
+
+x = random.binomial(n=10, p=0.5, size=10)
+
+print(x)
+
+
+"""
+Visualization of Binomial Distribution
+Example
+"""
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.binomial(n=10, p=0.5, size=1000), hist=True, kde=False)
+
+plt.show()
+
+
+
+"""
+Difference Between Normal and Binomial Distribution
+"""
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.distplot(random.normal(loc=50, scale=5, size=1000), hist=False, label='normal')
+sns.distplot(random.binomial(n=100, p=0.5, size=1000), hist=False, label='binomial')
+
+plt.show()
