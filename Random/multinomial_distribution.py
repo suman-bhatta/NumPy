9327be93cf5d8c541f97ec4a1d3996612e116454
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-06 20:26:14 +0545

    Add example for generating and visualizing multinomial distributions using NumPy

---
+++ Random/multinomial_distribution.py
@@ -0,0 +1,10 @@
+"""
+Multinomial Distribution
+Example: Draw out a sample for dice roll:
+"""
+
+from numpy import random
+
+x = random.multinomial(n=6, pvals=[1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
+
+print(x)
