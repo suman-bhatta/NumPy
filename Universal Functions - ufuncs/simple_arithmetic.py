e9811b22ee921e1280d683eb47de609686a7043b
Suman Bhatta <sumanbhatta735@gmail.com>
2025-01-07 21:20:24 +0545

    Add examples for basic arithmetic operations using NumPy ufuncs

---
+++ Universal Functions - ufuncs/simple_arithmetic.py
@@ -0,0 +1,107 @@
+"Addition"
+
+import numpy as np
+
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
+
+
+"Subtraction"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.subtract(arr1, arr2)
+
+print(newarr)
+
+
+"Multiplication"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.multiply(arr1, arr2)
+
+print(newarr)
+
+
+"Division"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 5, 10, 8, 2, 33])
+
+newarr = np.divide(arr1, arr2)
+
+print(newarr)
+
+
+"Power"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 5, 6, 8, 2, 33])
+
+newarr = np.power(arr1, arr2)
+
+print(newarr)
+
+
+
+"Remainder"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 7, 9, 8, 2, 33])
+
+newarr = np.mod(arr1, arr2)
+
+print(newarr)
+
+
+# Return the remainders
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 7, 9, 8, 2, 33])
+
+newarr = np.remainder(arr1, arr2)
+
+print(newarr)
+
+
+
+"Quotient and Mod"
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 7, 9, 8, 2, 33])
+
+newarr = np.divmod(arr1, arr2)
+
+print(newarr)
+
+
+"Absolute Values"
+
+import numpy as np
+
+arr = np.array([-1, -2, 1, 2, 3, -4])
+
+newarr = np.absolute(arr)
+
+print(newarr)
